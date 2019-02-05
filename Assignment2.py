a18410667f108c4891b4ec7354c72335be8ab191
Joshua Swick <joshuaswickirl@gmail.com>
2019-02-05 16:41:39 -0700

    Added graphFunctions to main, adjusted graphFunction to adjust list length

--- Assignment2.py
+++ Assignment2.py
@@ -9,6 +9,10 @@
 import time
 
 
+#
+#   Comparison functions and output data to csv
+#
+
 def functionTimer(f, input):
     """
     Returns the approximate runtime of f(input)
@@ -19,20 +23,26 @@ def functionTimer(f, input):
     return time2 - time1
 
 
-def graphFunction(f, input_start, input_end, numPoints, output_file_name):
+def graphFunction(f, input_data, input_start, input_end, numPoints, output_file_name):
     """
     Writes input, runtimes of f(input) for numPoints 
     to output_file_name.
     """
     file_out = open(output_file_name, "w+")
-    input_value = input_start  # Init input
+    end_of_splice = 1
+    input_value = input_data[input_start-1:end_of_splice]  # Init input
     increment = (input_end-input_start) // numPoints
     for i in range(numPoints):
-        dataList = listConverter(input_value)
-        runtime = functionTimer(f, dataList)
-        file_out.write(f"{str(input_value)},{str(runtime)}\n")
-        input_value += increment
+        #dataList = listConverter(input_value)
+        runtime = functionTimer(f, input_value)
+        file_out.write(f"{str(len(input_data))},{str(runtime)}\n")
+        end_of_splice += increment
     file_out.close()
+
+
+#
+#   Create data structures
+#
 
 def listConverter(amount):
     values = []
@@ -44,6 +54,29 @@ def listConverter(amount):
             else:
                 values.append(line.strip())
                 times += 1
+
+def createDictionary(fileName,numItems):
+    #Create counter variable and initiate dictionary
+    dictionary = []
+    count = 0
+    #open the file and read lines
+    with open(fileName) as f:
+        words = f.read().splitlines()
+    #creat a loop that adds to the dictionary 
+    for i in words:
+        temp ={count: i}
+        dictionary.append(temp)
+        count +=1
+        #check to see if you have reached your number of items
+        if  count == numItems:
+            break
+    #return the dictionary
+    return dictionary
+
+
+#
+#   Things to do to list
+#
 
 def listFunc1(data):
     return data.append("foo")
@@ -71,51 +104,45 @@ def numOfElements(data):
         numOfElements += 1
     return numOfElements
 
-def createDictionary(fileName,numItems):
-    #Create counter variable and initiate dictionary
-    dictionary = []
-    count = 0
-    #open the file and read lines
-    with open(fileName) as f:
-        words = f.read().splitlines()
-    #creat a loop that adds to the dictionary 
-    for i in words:
-        temp ={count: i}
-        dictionary.append(temp)
-        count +=1
-        #check to see if you have reached your number of items
-        if  count == numItems:
-            break
-    #return the dictionary
-    return dictionary
+#
+#   Things to do to dictionaries
+#
 
-def testTimeFunction(f1, input1, input11, f2, input2, input22):
-    #Function 1 times
-    startf1 = time.time()
-    f1(input1)
-    endf1 = time.time()
-    #Function 2 times
-    startf2 = time.time()
-    f2(input2, input22)
-    endf2 = time.time()
-    #total times
-    totaltimef1 = endf1-startf1
-    totaltimef2 = endf2-startf2
-    #compare total times
-    finalTimes = {f1:totaltimef1, f2: totaltimef2}
-    return finalTimes
+def dictFunc1(data):
+    pass
+
+
+
 
 def main():
-    # dataSet1 = []
-    # dataSet2 = []
-    # dataSet3 = []
-    # dataSet4 = {x for x in range(10**3)}
-    # dataSet5 = {x for x in range(10**4)}
-    # dataSet6 = {x for x in range(10**5)}
 
-    graphFunction(listFunc1, 1, 1000, 10, "graph.csv")
-    graphFunction(listFunc2, 1, 113809, 15, "graph1.csv")
-    graphFunction(listFunc3, 1, 113809, 15, "graph2.csv")
+    # List and Dict preparation
+    file_name = "words.txt"
+    length_of_list = 100000
+
+    # Create List from file
+    print("Creating list...")
+    list_data_structure = listConverter(length_of_list)
+    #print(list_data_structure)
+
+    # Create dict from file
+    print("Creating dictionary")
+    dict_data_structure = createDictionary(file_name,length_of_list)
+    #print(dict_data_structure)
+
+    # Get graph data for different functions
+    graphFunction(listFunc1, list_data_structure, 1, length_of_list, 15, "listFunc1.csv")
+    graphFunction(listFunc1, dict_data_structure, 1, length_of_list, 15, "listFunc1.csv")
+    
+    graphFunction(listFunc2, list_data_structure, 1, length_of_list, 15, "listFunc2.csv")
+    #graphFunction(listFunc2, dict_data_structure, 1, length_of_list, 15, "listFunc2.csv")
+
+    graphFunction(listFunc3, list_data_structure, 1, length_of_list, 15, "listFunc3.csv")
+    #graphFunction(listFunc3, dict_data_structure, 1, length_of_list, 15, "listFunc3.csv")
+
+    graphFunction(getFirstElement, list_data_structure, 1, length_of_list, 15, "getFirstElement.csv")
+    graphFunction(getFirstElement, dict_data_structure, 1, length_of_list, 15, "getFirstElement.csv")
+
 
 if __name__ == "__main__":
     main()
